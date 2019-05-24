67ee28a65b80f86b5a8a11d657c165683170211b
Patrick J. Sparrow <psparrow25@gmail.com>
2019-05-23 23:37:32 -0400

    Setup empty feature

---
+++ spec/features/creating_a_task_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'task management' do
+  scenario 'creating a task' do
+    pending
+    raise 'time for some TDD!'
+  end
+end
